f842da9ce799856449a522a8e9696f727841bb15
Erick Wendel <erick.workspace@gmail.com>
2024-08-28 12:10:33 -0500

    add mocking
    Signed-off-by: Erick Wendel <erick.workspace@gmail.com>

--- test/web.test.js
+++ test/web.test.js
@@ -1,7 +1,50 @@
-import { describe, it } from 'node:test'
+import { describe, it, before } from 'node:test'
+import Controller from '../src/shared/controller.js';
+import View from './../src/platforms/web/view.js'
+
+function getDocument(mock) {
+    globalThis.alert = mock.fn()
+
+    globalThis.document = {
+        createElement: mock.fn((name) => ({
+            classList: {
+                add: mock.fn((name) => { })
+            }
+        })),
+        querySelector: mock.fn((id) => {
+            return {
+                addEventListener: mock.fn((event, fn) => {
+
+                    return fn({
+                        preventDefault: () => { }
+                    })
+                }),
+                reset: mock.fn(() => {
+
+                }),
+                appendChild: mock.fn((child) => {
+                    return {
+
+                    }
+                })
+            }
+        }),
+    }
+
+    return globalThis.document
+}
 
 describe('Web app test suite', () => {
-    it('given valid input, should update the table data', async () => {
-        console.log('working');
+    let _controller
+    before(() => {
+
+    })
+    it('given valid input, should update the table data', async (context) => {
+        const document = getDocument(context.mock)
+        _controller = Controller.init({
+            view: new View()
+        })
+
+        document.querySelector.mock
     })
 })
