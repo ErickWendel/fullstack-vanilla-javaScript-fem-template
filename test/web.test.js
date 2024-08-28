3e8b33a38172163e609f4829ccb5d2b78908b6fe
Erick Wendel <erick.workspace@gmail.com>
2024-08-28 11:42:43 -0500

    add vscode debugger and test setup
    Signed-off-by: Erick Wendel <erick.workspace@gmail.com>

---
+++ test/web.test.js
@@ -0,0 +1,7 @@
+import { describe, it } from 'node:test'
+
+describe('Web app test suite', () => {
+    it('given valid input, should update the table data', async () => {
+        console.log('working');
+    })
+})
